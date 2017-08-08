9db2c9c43dd51b2d0d70ef8b23d3917aa49c18cf
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-08-08 18:47:23 -0500

    attempted to style around the fixed header

--- header.php
+++ header.php
@@ -44,8 +44,10 @@
 			<?php
 			endif; ?>
 		</div><!-- .site-branding -->
-
+		<div class="navigation-top">
+		<div class="wrap">
 		<nav id="site-navigation" class="navbar navbar-toggleable-md navbar-light bg-faded">
+
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			    <span class="navbar-toggler-icon"></span>
 			  </button>
@@ -60,6 +62,8 @@
 				) );
 			?>
 		</nav><!-- #site-navigation -->
+		</div>
+	</div>
 	</header><!-- #masthead -->
 
 	<div id="content">
