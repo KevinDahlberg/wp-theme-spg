9ecea471ea57f724de4029c8fefcdedc13b34a3b
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-08-09 19:48:07 -0500

    styled about panel with multiple columns for the text and media queries

--- front-page.php
+++ front-page.php
@@ -10,25 +10,37 @@
 get_header(); ?>
 
 <div class="container-fluid">
-  <div id="about-panel" class="row">
-    <?php
-          $mypages = get_pages( array( 'child_of' => $post->ID ) );
-          //get's pages that are children of the home page
-          foreach( $mypages as $page ) {
-            $content = $page->post_content;
-              if ( ! $content ) // Check for empty page
-                continue;
-              $content = apply_filters( 'the_content', $content );
-        ?>
+  <div id="about-panel" class="row justify-content-cente">
+    <div class="col">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="row">
+            <div class="col">
+              <?php
+                $mypages = get_pages( array( 'child_of' => $post->ID ) );
+                //get's pages that are children of the home page
+                foreach( $mypages as $page ) {
+                  $content = $page->post_content;
+                    if ( ! $content ) // Check for empty page
+                      continue;
+                    $content = apply_filters( 'the_content', $content );
+              ?>
 
-        <h2><a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a></h2>
-    <div class="col">
-      <?php echo get_the_post_thumbnail( $page->ID, 'full', ['class' => 'img-responsive front-about-image']); ?>
+              <h2><a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a></h2>
+            </div>
+          </div>
+         <div class="row">
+           <div class="col about-image">
+             <?php echo get_the_post_thumbnail( $page->ID, 'full', ['class' => 'img-fluid about-image']); ?>
+           </div>
+         </div>
+     </div>
+        <div class="col-lg-6 text-columns">
+          <?php echo $content; ?>
+        </div>
+            <?php } ?>
+      </div>
     </div>
-    <div class="col">
-      <?php echo $content; ?>
-    </div>
-    <?php } ?>
   </div>
 </div>
 <?php
