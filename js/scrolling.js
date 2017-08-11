732376154f2511570d0973e0c6f2247853109675
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-08-11 16:39:38 -0500

    polishing up scrolling.js

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,56 +1,58 @@
+/**
+ * @desc javascript file that sets up parallax scrolling
+ * @package spg
+ * @version 1.0
+ */
+
 (function($) {
 
+  /**
+   * @desc app setup
+   */
   sizing();
-
+  sticky();
   $(window).resize(sizing);
 
-  sticky();
 
   /**
-  * @function sizing
-  * @desc functions that are run when the window resizes
-  */
-  function sizing () {
+   * @function sizing
+   * @desc functions that are run when the window resizes
+   */
+  function sizing() {
     sticky();
   }
 
   /**
-  * @function sticky
-  * @desc sets the divs that are going to stick to the top of the screen on scroll
-  */
-  function sticky () {
+   * @function sticky
+   * @desc sets the divs that are going to stick to the top of the screen on scroll
+   */
+  function sticky() {
     var navSticky = $('#site-navigation');
     var firstSticky = $('#about-panel');
     var secondSticky = $('#shows-panel-image');
-    stickyDiv (navSticky);
+    stickyDiv(navSticky);
     // stickyDiv (firstSticky);
-    stickyDiv (secondSticky);
+    stickyDiv(secondSticky);
   }
 
   /**
-  * @function stickyDiv
-  * @desc adds an affix class to the provided element
-  * @param element, either an ID or a class
-  * @return adds affix class, CSS in the stylesheet needs to match up
-  */
-  function stickyDiv (el) {
+   * @function stickyDiv
+   * @desc adds an affix class to the provided element
+   * @param element, either an ID or a class
+   * @return adds affix class, CSS in the stylesheet needs to match up
+   */
+  function stickyDiv(el) {
     var sticky = el.offset().top;
     $(window).scroll(function() {
-        // console.log('scroll ', $(window).scrollTop());
-        // console.log('sticky ', sticky);
-        if ($(window).scrollTop() > (sticky)) {
-            el.addClass('affix');
-        } else {
-          el.removeClass('affix');
-        }
+      // console.log('scroll ', $(window).scrollTop());
+      // console.log('sticky ', sticky);
+      if ($(window).scrollTop() > (sticky)) {
+        el.addClass('affix');
+      } else {
+        el.removeClass('affix');
+      }
 
     });
   }
 
 })(jQuery);
-
-
-
-function navBar () {
-
-}
